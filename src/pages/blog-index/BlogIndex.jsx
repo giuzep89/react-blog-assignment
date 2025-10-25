62f09105d532f8b3229f2a0a365a555c88fde104
giuzep89 <g.rinaldimusic@gmail.com>
2025-10-25 13:28:40 +0200

    Fixes

--- src/pages/blog-index/BlogIndex.jsx
+++ src/pages/blog-index/BlogIndex.jsx
@@ -5,16 +5,16 @@ import axios from "axios";
 
 
 function BlogIndex() {
-    const [posts, setPosts] = useState([]);
     const [error, setError] = useState(false);
     const navigate = useNavigate();
+    const [allPosts, setAllPosts] = useState([]);
 
     function goToPost(postId) {
         navigate(`/posts/${postId - 1}`);
     }
 
     useEffect(() => {
-        async function fetchAllPosts() {
+        async function getAllPosts() {
             setError(false);
             try {
                 const result = await
@@ -22,37 +22,40 @@ function BlogIndex() {
                         {
                             headers: {'novi-education-project-id': 'c5b1327a-6c34-419a-8701-6b842cba268c'}
                         });
-                setPosts(result.data);
+                setAllPosts(result.data);
                 console.log(result);
             } catch (e) {
                 console.error(e);
                 setError(true);
             }
         }
-
-        fetchAllPosts();
+        getAllPosts();
     }, []);
+
+    if (error) {
+        return (
+            <main className="outer-container">
+                <p className="error-message">Er is een fout opgetreden :(</p>
+            </main>
+        );
+    }
 
     return (
         <>
             <main className="outer-container">
-
-                {error ? <p style={{color: "red"}}>Er is een fout opgetreden :(</p> :
-                    <>
-                        <h1>Bekijk alle {posts.length} posts op het platform</h1>
-                        <div className="inner-container posts-list">
-                            {Object.keys(posts).length > 0 &&
-                                posts.map((post) => {
-                                    return <article key={post.id}>
-                                        <h2 onClick={() => {
-                                            goToPost(post.id)
-                                        }}>{post.title} <span>({post.author})</span></h2>
-                                        <p>{post.comments} reacties - {post.shares} keer gedeeld</p>
-                                    </article>
-                                })
-                            }
-                        </div>
-                    </>}
+                <h1>Bekijk alle {allPosts.length} posts op het platform</h1>
+                <div className="inner-container posts-list">
+                    {allPosts.length > 0 &&
+                        allPosts.map((post) => {
+                            return <article key={post.id}>
+                                <h2 onClick={() => {
+                                    goToPost(post.id)
+                                }}>{post.title} <span>({post.author})</span></h2>
+                                <p>{post.comments} reacties - {post.shares} keer gedeeld</p>
+                            </article>
+                        })
+                    }
+                </div>
             </main>
         </>
     )
--- src/pages/blog-post/BlogPost.jsx
+++ src/pages/blog-post/BlogPost.jsx
@@ -1,16 +1,57 @@
 import {Link, useParams} from "react-router-dom"
-import posts from "../../constants/data.json"
+import {useEffect, useState} from "react";
 import {formatDate} from "../../helpers/formatDate.js";
 import Footer from "../../components/footer/Footer.jsx";
 import "./BlogPost.css"
+import axios from "axios";
 
 function BlogPost() {
     const {id} = useParams();
-    const {title, subtitle, author, created, content, comments, shares, readTime} = posts[id];
+    const [error, setError] = useState(false);
+    const [post, setPost] = useState({});
+    const {
+        title = "",
+        subtitle = "",
+        author = "",
+        created = "",
+        content = "",
+        comments = 0,
+        shares = 0,
+        readTime = 0
+    } = post;
+
+    useEffect(() => {
+        async function getPostById() {
+            setError(false);
+            try {
+                const response = await
+                    axios.get(`https://novi-backend-api-wgsgz.ondigitalocean.app/api/blogposts/${id}`,
+                        {
+                            headers: {'novi-education-project-id': 'c5b1327a-6c34-419a-8701-6b842cba268c'}
+                        });
+                setPost(response.data);
+                console.log(response.data);
+            } catch (e) {
+                console.error(e);
+                setError(true);
+            }
+        }
+        getPostById();
+    }, []);
+
+
+    // Down here I implement a different way of handling an error in fetching the data compared to
+    // what I used in BlogIndex.jsx (there I use an if-statement construction),
+    // purely to experiment with a different solution! The if-construction is still probably
+    // more readable than this, because you clearly say: if this happens return THIS, otherwise
+    // return THAT.
 
     return (
         <>
             <main className="outer-container blog-post">
+                {error && <p className="error-message">Er is een fout opgetreden :(</p>}
+
+                {!error && (
                 <div className="inner-container">
                     <h1>{title}</h1>
                     <h2>{subtitle}</h2>
@@ -20,6 +61,7 @@ function BlogPost() {
                     <p className="comments-shares">{comments} reacties - {shares} keer gedeeld</p>
                     <Link to="/index" className="go-back-link"> ⟨ Terug naar de overzichtpagina</Link>
                 </div>
+                )}
             </main>
             <Footer/>
         </>
